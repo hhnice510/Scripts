efb736cce134be692fb4176a9a3e495d9a896470
hhnice <65580234+hhnice510@users.noreply.github.com>
2022-04-25 16:58:11 +0800

    Create python3ide.js

---
+++ python3ide.js
@@ -0,0 +1,150 @@
+/*
+*
+*
+Python3IDE修改订阅
+by nice
+使用声明：⚠️⚠️⚠️此脚本仅供学习与交流，严禁转载与贩卖！⚠️⚠️⚠️
+
+*********************
+
+[rewrite_local]
+
+# Python3IDE解锁订阅
+^https?:\/\/api\.revenuecat\.com\/v1\/subscribers\/\$\S{48} url script-response-body python3ide.js
+
+[mitm]
+
+hostname = api.revenuecat.com
+
+*
+*
+*/
+
+
+
+var _0xodP = 'jsjiami.com.v6',
+    _0xodP_ = ['‮_0xodP'],
+    _0x1c9e = [_0xodP, 'wp/ChsKswrASwpJGSBcjw7vCqB8=', 'w5Mlw5Q7I8Onw7p0LcKTwpbDscKeKTwpE8O3w4Bn', 'wp56wr4KZj/DvBjDvA==', 'wpTCvHN0Fg==', 'bhHCuToMwprCr8OQwq0afUrDrMK6w4EDRcKtwqN5', 'E8KIw43CqSvCl8KqHMO/', 'w60jdipmTjdCw5A=', 'MFPCjMKaWw==', 'wpJ+wr8cwqvDr0vDpMOiLQ==', 'wozDoR/Ch8OMwrfDksKYw4DDusKYaMKuw7/DnEcWUn0w', 'w7fDhQDDvMKyN1PCmcKLw5/CgyrCs8KRfsOVOA==', 'w5DCnMKMJgBiwobDvkDDusKSNxfCoMKjQcK6w6MmwqA=', 'wpJgwoR6bcKlwqc7esOU', 'F0XClcKVw65EAcOHEMKTOCYgMsK8w5XCslvCp19+dHDCgyrCssOTTy/DkcK2HsKCGcOUMjHCnAEqW8OKwpFj', 'jsjiamxCi.JcoOm.Dv6kICHbwgWP=='];
+if (function(_0x596965, _0x1138f9, _0x4a52b8) {
+    function _0x5779e4(_0x89bb4a, _0x2f024f, _0x3f0167, _0x47f8ca, _0x2a637e, _0x58e91f) {
+        _0x2f024f = _0x2f024f >> 0x8, _0x2a637e = 'po';
+        var _0x332011 = 'shift',
+            _0x1a6882 = 'push',
+            _0x58e91f = '‮';
+        if (_0x2f024f < _0x89bb4a) {
+            while (--_0x89bb4a) {
+                _0x47f8ca = _0x596965[_0x332011]();
+                if (_0x2f024f === _0x89bb4a && _0x58e91f === '‮' && _0x58e91f['length'] === 0x1) {
+                    _0x2f024f = _0x47f8ca, _0x3f0167 = _0x596965[_0x2a637e + 'p']();
+                } else if (_0x2f024f && _0x3f0167['replace'](/[xCJODkICHbwgWP=]/g, '') === _0x2f024f) {
+                    _0x596965[_0x1a6882](_0x47f8ca);
+                }
+            }
+            _0x596965[_0x1a6882](_0x596965[_0x332011]());
+        }
+        return 0xe1a9e;
+    };
+    return _0x5779e4(++_0x1138f9, _0x4a52b8) >> _0x1138f9 ^ _0x4a52b8;
+}(_0x1c9e, 0x181, 0x18100), _0x1c9e) {
+    _0xodP_ = _0x1c9e['length'] ^ 0x181;
+};
+
+function _0x3312(_0x3d2be4, _0x5f1ba9) {
+    _0x3d2be4 = ~~'0x' ['concat'](_0x3d2be4['slice'](0x1));
+    var _0x42208f = _0x1c9e[_0x3d2be4];
+    if (_0x3312['DfssaE'] === undefined) {
+        (function() {
+            var _0x4f8807;
+            try {
+                var _0x316675 = Function('return (function() ' + '{}.constructor("return this")( )' + ');');
+                _0x4f8807 = _0x316675();
+            } catch (_0x108547) {
+                _0x4f8807 = window;
+            }
+            var _0xe3e4c0 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            _0x4f8807['atob'] || (_0x4f8807['atob'] = function(_0x17c9d1) {
+                var _0x27db74 = String(_0x17c9d1)['replace'](/=+$/, '');
+                for (var _0x320a97 = 0x0, _0x5b146e, _0x16306e, _0x1c6aef = 0x0, _0x117eed = ''; _0x16306e = _0x27db74['charAt'](_0x1c6aef++); ~_0x16306e && (_0x5b146e = _0x320a97 % 0x4 ? _0x5b146e * 0x40 + _0x16306e : _0x16306e, _0x320a97++ % 0x4) ? _0x117eed += String['fromCharCode'](0xff & _0x5b146e >> (-0x2 * _0x320a97 & 0x6)) : 0x0) {
+                    _0x16306e = _0xe3e4c0['indexOf'](_0x16306e);
+                }
+                return _0x117eed;
+            });
+        }());
+
+        function _0x1626ad(_0x2b2280, _0x5f1ba9) {
+            var _0x5fe74c = [],
+                _0x3260c8 = 0x0,
+                _0x500b86, _0xddb439 = '',
+                _0x16f4db = '';
+            _0x2b2280 = atob(_0x2b2280);
+            for (var _0x5b48c9 = 0x0, _0x3e827e = _0x2b2280['length']; _0x5b48c9 < _0x3e827e; _0x5b48c9++) {
+                _0x16f4db += '%' + ('00' + _0x2b2280['charCodeAt'](_0x5b48c9)['toString'](0x10))['slice'](-0x2);
+            }
+            _0x2b2280 = decodeURIComponent(_0x16f4db);
+            for (var _0x468220 = 0x0; _0x468220 < 0x100; _0x468220++) {
+                _0x5fe74c[_0x468220] = _0x468220;
+            }
+            for (_0x468220 = 0x0; _0x468220 < 0x100; _0x468220++) {
+                _0x3260c8 = (_0x3260c8 + _0x5fe74c[_0x468220] + _0x5f1ba9['charCodeAt'](_0x468220 % _0x5f1ba9['length'])) % 0x100;
+                _0x500b86 = _0x5fe74c[_0x468220];
+                _0x5fe74c[_0x468220] = _0x5fe74c[_0x3260c8];
+                _0x5fe74c[_0x3260c8] = _0x500b86;
+            }
+            _0x468220 = 0x0;
+            _0x3260c8 = 0x0;
+            for (var _0x51ab7f = 0x0; _0x51ab7f < _0x2b2280['length']; _0x51ab7f++) {
+                _0x468220 = (_0x468220 + 0x1) % 0x100;
+                _0x3260c8 = (_0x3260c8 + _0x5fe74c[_0x468220]) % 0x100;
+                _0x500b86 = _0x5fe74c[_0x468220];
+                _0x5fe74c[_0x468220] = _0x5fe74c[_0x3260c8];
+                _0x5fe74c[_0x3260c8] = _0x500b86;
+                _0xddb439 += String['fromCharCode'](_0x2b2280['charCodeAt'](_0x51ab7f) ^ _0x5fe74c[(_0x5fe74c[_0x468220] + _0x5fe74c[_0x3260c8]) % 0x100]);
+            }
+            return _0xddb439;
+        }
+        _0x3312['VdfWzo'] = _0x1626ad;
+        _0x3312['qNcQxT'] = {};
+        _0x3312['DfssaE'] = !![];
+    }
+    var _0x2e5b57 = _0x3312['qNcQxT'][_0x3d2be4];
+    if (_0x2e5b57 === undefined) {
+        if (_0x3312['snKiFb'] === undefined) {
+            _0x3312['snKiFb'] = !![];
+        }
+        _0x42208f = _0x3312['VdfWzo'](_0x42208f, _0x5f1ba9);
+        _0x3312['qNcQxT'][_0x3d2be4] = _0x42208f;
+    } else {
+        _0x42208f = _0x2e5b57;
+    }
+    return _0x42208f;
+};
+var body = $response['body'];
+var obj = JSON[_0x3312('‫0', 'PCbJ')](body);
+obj[_0x3312('‫1', 'ak1N')]['entitlements'] = {
+    'pro': {
+        'expires_date': _0x3312('‮2', 'CvDq'),
+        'grace_period_expires_date': null,
+        'product_identifier': _0x3312('‮3', 'XON3'),
+        'purchase_date': _0x3312('‮4', 'OWnt')
+    }
+};
+
+
+obj[_0x3312('‫5', '2N&x')]['management_url'] = _0x3312('‫6', 'zrPM');
+obj['subscriber'][_0x3312('‫7', 'P(r2')] = {
+    'python3ide_annual': {
+        'billing_issues_detected_at': null,
+        'expires_date': '2032-05-02T06:21:11Z',
+        'grace_period_expires_date': null,
+        'is_sandbox': ![],
+        'original_purchase_date': _0x3312('‮8', '2N&x'),
+        'ownership_type': _0x3312('‫9', 'x@Xd'),
+        'period_type': _0x3312('‫a', 'rN67'),
+        'purchase_date': _0x3312('‮b', 'IPml'),
+        'store': _0x3312('‮c', 'RrAK'),
+        'unsubscribe_detected_at': null
+    }
+};
+body = JSON[_0x3312('‫d', '[9Vt')](obj);
+$done(body);;
+_0xodP = 'jsjiami.com.v6';
