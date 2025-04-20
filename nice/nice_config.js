d963f97cbcd9e41c5cd6eb64fef19db7d31f7d69
hhnice <65580234+hhnice510@users.noreply.github.com>
2025-04-20 22:35:35 +0800

    Create nice_config.js

---
+++ nice/nice_config.js
@@ -0,0 +1,25 @@
+/*
+********
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/www\.example\.com url script-response-body https://raw.githubusercontent.com/hhnice510/Scripts/refs/heads/main/nice/nice_config.js
+^https?:\/\/haijiao\.com url script-response-body https://raw.githubusercontent.com/hhnice510/Scripts/refs/heads/main/nice/nice_config.js
+
+[mitm]
+hostname = www.example.com, haijiao.com
+
+
+*/
+
+var body_regex = /<\/head>/gi;
+var body_replace_str = '<link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/all.min.css" rel="stylesheet">\
+<script src="https://cdn.jsdelivr.net/gh/hhnice510/Scripts@refs/heads/main/nice/nicebtn.js"></script></head>\
+';
+
+
+if ($response.body !== null || $response.body !== undefined) {  // 判断响应体是否存在
+    var body = $response.body.replaceAll(body_regex, body_replace_str);
+}
+
+$done({body: body});
